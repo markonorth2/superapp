d6f97ce545d70481fe98684b7753af70a95a5cf2
Mark Chuang <markchuangth@gmail.com>
2022-04-23 10:58:07 -0400

    refactor code to use useReducer instead of useState to manage the counter

--- client/src/components/UseReducer.js
+++ client/src/components/UseReducer.js
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
+
+function reducer(state, action) {
+  return {count: state.count +1} 
+}
+
 
 function UseReducer() {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0})
 
-  function changeCount(amount) {
-    setCount(prevCount => prevCount + amount)
+
+  function increment(){
+    dispatch()
   }
 
-  function resetCount() {
-    setCount(0)
+  function decrement(){
+
   }
+
+
   return (
     <div>
-     <span>{count}</span>
-      <button onClick={() => changeCount(1)}>+</button>
-      <button onClick={() => changeCount(-1)}>-</button>
-      <button onClick={() => resetCount()}>Reset</button>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>-</button>
     </div>
   );
 }
